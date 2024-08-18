7efb3982ae14aa5f061250a2ef98671c54df4de1
TheoCaesar <theocaesar7@gmail.com>
2024-08-18 04:03:56 +0100

    multiple params with pipes

--- src/app/shorten.pipe.ts
+++ src/app/shorten.pipe.ts
@@ -4,9 +4,9 @@ import { Pipe, PipeTransform } from "@angular/core";
     name:"truncate" //template variable to invoke use in template
 })
 export class ShortenPipe implements PipeTransform {
-    transform(value: string, length:number = 10) {
+    transform(value: string, length:number = 10, startIndex: number = 0) {
         if (value.length > length) {
-           return value.substring(0, length) + '...'
+           return value.substring(startIndex, length) + '...'
         }
         return value;
     }
